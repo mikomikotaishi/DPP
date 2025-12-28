133c71409f6932edd52a6e2342dfb4257e4219d8
Toyosatomimi no Miko <110693261+mikomikotaishi@users.noreply.github.com>
2025-12-27 21:00:27 -0500

    Add modules unit test

--- docpages/example_code/using_modules.cpp
+++ docpages/example_code/using_modules.cpp
@@ -1,22 +1,21 @@
-/* If import std; is supported, use it instead */
-#include <cstdlib>
-
 import dpp;
 
 using dpp::cluster;
+using std::ready_t;
 using dpp::slashcommand;
+using dpp::slashcommand_t;
 using dpp::start_type;
 
 int main() {
-	cluster bot(std::getenv("BOT_TOKEN"));
+	cluster bot("YOUR_BOT_TOKEN_HERE");
 
-	bot.on_slashcommand([](auto event) {
+	bot.on_slashcommand([](const slashcommand_t& event) -> void {
 		if (event.command.get_command_name() == "ping") {
 			event.reply("Pong!");
 		}
 	});
 
-	bot.on_ready([&bot](auto event) {
+	bot.on_ready([&bot](const ready_t& event) -> void {
 		if (dpp::run_once<struct register_bot_commands>()) {
 			bot.global_command_create(
 				slashcommand("ping", "Ping pong!", bot.me.id)
